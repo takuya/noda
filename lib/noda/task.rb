7e59c3409bd620c20bd4b3d294791a25f08b3680
takuya <takuya.1st@gmail>
2011-06-03 15:45:55 +0900

    pid ファイルの位置を変えた./var/run はパーミッションErrorになる

--- lib/noda/task.rb
+++ lib/noda/task.rb
@@ -1,10 +1,6 @@
 module Noda
 
 class Task
-  #QÆ“n‚µ‚É‚µ‚½‚¢‚Æ‚«‚Í ‚±‚Ì include ‚ğg‚¤
-  #  objects-by-reference
-  #def by_reference() include DRb::DRbUndumped end
-  
   #this should be overwrite
   def do_task(hash_table)
   end
