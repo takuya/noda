19ecd176560db53b1c4c5a09b00b41ca3a6b7540
takuya <takuya.1st@gmail>
2011-06-04 00:58:42 +0900

    コメント書いた

--- lib/noda/rqueue.rb
+++ lib/noda/rqueue.rb
@@ -2,13 +2,20 @@
 
 module Noda
 require 'monitor'
-# �W���u�̃L���[����
-# �L���[�̓X���b�h�Z�[�t�ɏ����Ă���
-#
+# ジョブのキュー実装
+# キューはスレッドセーフに書いている
+# 
+# キューから値を取り出すと、キューには残らない。
+# ==使用法
+#     require 'noda'
+#     server =Noda::JobServer.new
+#     q = server.input #<= Jobサーバーが持ってる
+#     q.push Noda::MyTask.new("hogehgoe")
 #
 class RQueue
   include DRb::DRbUndumped 
   attr_reader :name
+  # 
   def initialize( max=nil, name=nil )
     @name = name
     @list = []
@@ -18,6 +25,9 @@ class RQueue
     @m_empty = self.new_cond
     @m_full = self.new_cond
   end
+  # キュー末尾にオブジェクトを追加。
+  #
+  # キュー満杯時は実行スレッドをWaitさせます。
   def push obj
     self.synchronize{
       @m_full.wait_while{ self.full?  } if @max
@@ -25,6 +35,9 @@ class RQueue
       @m_empty.broadcast
     }
   end
+  # キュー先頭からオブジェクトを取り出す．
+  #
+  # キュー空なら実行スレッドをWaitさせる．
   def pop
     self.synchronize{
       @m_empty.wait_while{ self.empty?  }
@@ -33,20 +46,30 @@ class RQueue
       obj
     }
   end
+  # 実験用メソッド・使わない．
   def include?(v) @list.include? v        end
   alias exists? include?
+  # キューの先頭N個を取り出す．
   def firsts(n=1) (0...n).map{self.pop}   end
+  # キューの先頭１個を取り出す． pop の別名
   def first()     self.pop                end
+  # キューのサイズを取得
   def size()      @list.size              end
+  # キュー格納可能数
   def max_size()  @max                    end
+  # キューに値があるか
   def empty?()    @list.empty?            end
+  # キュー満杯が近いときにTrueを返す．
   def close_to_full?()    @list.size >= @max-5    end
+  # キューが満杯かどうか
   def full?()
-    # max=nil �Ȃ�Limitless�B�܂薳����
+    # max=nil ならLimitless。つまり無限大
     @list.size >= @max if @max
   end
+  # キューの値全てを取り出す．
   def all() self.firsts(self.size) end
-  # �P����m�܂ł̒l�����B�O�n�܂�łȂ����Ƃɒ���
+  # キューの先頭N番目の値を調べる．チェック用。
+  # - pos １からＮまでの値を取る。先頭は1 で指定する．０始まりでないことに注意
   def _at(pos) 
     i = pos - 1 
     return @list.at(i) if (i) < self.size 
--- lib/noda/table.rb
+++ lib/noda/table.rb
@@ -1,10 +1,21 @@
 # 
 module Noda
-#スレッドセーフなHASHテーブルを作ってる
-#内部的にはHashにしている
-#KVSとして使っている
-#TODO::HASHとして動くようにEnumerableを実装したい
-#TODO::DBやファイルに保存する。このクラスAPIでアクセス出来るアダプタを作りたい
+#  スレッドセーフなHASHテーブルを作ってる
+#
+#  ジョブの共有ストレージ．
+#  内部的にはHashにしている
+#
+#  KVSとして使う事を想定
+#
+#     require 'noda'
+#     server =Noda::JobServer.new
+#     table = server.hash_table #<= 自分でインスタンス化しない．Jobサーバーから使う
+#     table.put "key, "value"
+#  タスクから使う場合。
+#     do_task(hash_table)
+#       v = hash_table.get "key"
+#       hash_table.put "key", "value2" # =>更新
+#     end
 class Table
   include DRb::DRbUndumped
   include Enumerable
@@ -16,34 +27,60 @@ class Table
     @m_lock = self.new_cond
     @saved_keys = []
   end
+  # キー対応した値を取り出す
   def get(key)
     @hash[key][:data]
   end
+  # キーを名前に値を保存する．
   def put(key, obj)
     self.synchronize{
       @hash[key] = {:data=>obj,:saved=>false}
     }
   end
+  # キーがテーブルに存在するか
   def has_key?(key) @hash.has_key? key end
+  # alias to has_key
   alias :exists? :has_key?
+  # キーを全部取得
   def keys() @hash.keys end
+  # テーブルの値の数
   def size() @hash.size end
+  # Enumerable 用。
+  #
+  # drb 経由で呼び出す時は、参照渡しになるので注意
   def each 
     @hash.each_pair{|k,v| yield( k,v[:data] )  }
     nil
   end
+  
+  # テーブルの値はDump済みかどうか
+  #
+  # 永続ストレージに書き出すときに
+  # このフラグを使って処理をする
   def saved?(key)
     @hash[key][:saved]
   end
+  # テーブルの値の保存状態を更新する．
+  #
+  # 永続ストレージに書き出すときに
+  # このフラグを使って処理をする
   def update_saved_at(key,status=true)
     self.synchronize{
       @hash[key][:saved]=status
     }
   end
+  # テーブルの未保存の値をすべて取り出す．」
+  #
+  # 永続ストレージに書き出すときに
+  # このメソッドを使って処理が出来る
   def each_unsaved_pair
     @hash.each_pair{|k,v| next if v[:saved]; yield( k, v[:data] )  }
     nil
   end
+  # 未保存の値があるか
+  #
+  # 永続ストレージに書き出すときに
+  # このフラグを使って処理をする
   def has_unsaved_key?
     return true if @hash.find{|k,v| v[:saved]==false}
     return false
